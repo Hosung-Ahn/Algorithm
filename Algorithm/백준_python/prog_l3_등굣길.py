5657abdbf4557c6b7a532d63737fa37a6dcb2404
Hosung <an3735297@pusan.ac.kr>
2022-09-24 13:05:41 +0900

    prog_l3_등굣길 수정
    functools.lru_cache() 데커레이터 를 활용하여
    dp 함수 구현

--- Algorithm/백준_python/prog_l3_등굣길.py
+++ Algorithm/백준_python/prog_l3_등굣길.py
@@ -1,32 +1,26 @@
+import functools
+
 def solution(m, n, puddles):
-    dp = [[-1 for _ in range(m)] for _ in range(n)]
     MOD = 1_000_000_007
+    puddle = [[0 for _ in range(m)] for _ in range(n)]
     for p in puddles :
-        dp[p[1]][p[0]] = -2
+        puddle[p[1]-1][p[0]-1] = -2
         
     def in_range(r,c) :
         if r<0 or r>=n or c<0 or c>=m : return False
         return True
         
+    @functools.lru_cache()
     def find_dp(r, c) :
-        nonlocal dp
-        if not in_range(r,c) or dp[r][c] == -2: return 0
+        if not in_range(r,c) or puddle[r][c] : return 0
         if r == 0 and c == 0 : return 1
-        if dp[r][c] != -1 : return dp[r][c]
         
         left = find_dp(r,c-1)
         up = find_dp(r-1,c)
         ret = (left+up) % MOD
-        dp[r][c] = ret
         return ret
         
-        
-    x = find_dp(n-1, m-1)
-    for r in range(3) :
-        for c in range(4) :
-            print(dp[r][c], end = ' ')
-        print()
-    return x
+    return find_dp(n-1, m-1)
 
 print(solution(4,3, [[2, 2]]))
  
