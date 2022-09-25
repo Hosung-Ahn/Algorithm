54cb16fe85f51c5a78df2f1fdd9d97403cabc86c
Hosung <an3735297@pusan.ac.kr>
2022-09-25 13:22:05 +0900

    BOJ_6987(back tracking)

---
+++ Algorithm/백준_python/BOJ.py
@@ -0,0 +1,29 @@
+from sys import stdin
+from itertools import combinations
+input = stdin.readline
+
+def solve(match) :
+    if match == 15 :
+        return 1
+    
+    t1, t2 = game[match]
+    
+    for i1,i2 in [[0,2], [1,1], [2,0]] :
+        if res[t1][i1] > 0 and res[t2][i2] > 0 : 
+            res[t1][i1] -= 1
+            res[t2][i2] -= 1
+            if solve(match+1) : return 1
+            res[t1][i1] += 1
+            res[t2][i2] += 1
+    return 0
+            
+    
+
+game = list(combinations(range(6), 2)) 
+ans = []
+for _ in range(4) :
+    data = list(map(int, input().split()))
+    res = [data[i:i+3] for i in range(0,16,3)]
+    ans.append(solve(0))
+
+print(*ans)
