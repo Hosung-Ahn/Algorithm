ecfcc4e0d7ec2681f40525428fb4e4ebd5a1a878
Hosung <an3735297@pusan.ac.kr>
2022-09-27 10:54:42 +0900

    prog_l3_기지국설치(array)

---
+++ Algorithm/백준_python/BOJ_2228.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+
+arr = []
+N,K = map(int, input().split())
+M = N-K+1
+
+for _ in range(N) :
+    x = int(input())
+    arr.append(x)
+
+ans = -987654321
+
+Sum = [0 for _ in range(N)]
+for i in range(N) :
+    if i >= M : break
+    for j in range(i,N) :
+        Sum[j-i] += arr[j]
+        if K==2 and i-j != 0 and j!=N-1:
+            continue
+        ans = max(ans, Sum[j-i])
+    print(Sum)
+        
+print(ans)
---
+++ Algorithm/백준_python/prog_l3_기지국설치.py
@@ -0,0 +1,22 @@
+import math
+
+def cal(length, w) : 
+    return math.ceil(length/(2*w+1))
+
+def solution(n, stations, w):
+    ans = 0
+    s = 1
+    for station in stations :
+        e = station-w-1
+        length = e-s+1
+        s = station+w+1
+        if length <= 0 : continue
+        ans += cal(length, w)
+        
+    if s <= n :
+        length = n-s+1
+        ans += cal(length, w)
+    return ans
+
+
+print(solution(3,[2],1))
