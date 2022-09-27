0a6c989b73ea16554bc410e665f3cd9801aaf152
Hosung <an3735297@pusan.ac.kr>
2022-09-27 14:11:12 +0900

    prog_l3_가장먼노드(bfs)

---
+++ Algorithm/백준_python/prog_l3_가장먼노드.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+def solution(n, edge):
+    edges = [[] for _ in range(n+1)]
+    dist = defaultdict(int)
+    
+    for a,b in edge :
+        edges[a].append(b)
+        edges[b].append(a)
+
+    def bfs() :
+        visited = [False for _ in range(n+1)]
+        visited[1] = True
+        q = [[1,0]]
+        while q :
+            c = q[0][0]
+            d = q[0][1]
+            q.pop(0)
+            
+            dist[d] += 1
+            
+            for next in edges[c] :
+                if visited[next] : continue
+                visited[next] = True
+                q.append([next,d+1])
+        
+    bfs()
+    mk = max(dist.keys())
+    print(dist)
+    return dist[mk]
+
+print(solution(6,[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]	))
