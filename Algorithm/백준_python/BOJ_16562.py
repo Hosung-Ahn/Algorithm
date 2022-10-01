bdef31a1e8a7eb36ada5fbf0e24bfc3dc0ac0ea4
Hosung <an3735297@pusan.ac.kr>
2022-10-01 11:16:06 +0900

    BOJ_16562(Disjoint Set)

---
+++ Algorithm/백준_python/BOJ_16562.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+N,M,k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+S = [i for i in range(N+1)]
+
+def get_parent(a) :
+    if a == S[a] : return a
+    S[a] = get_parent(S[a])
+    return S[a]
+
+def union(a, b) :
+    a = get_parent(a)
+    b = get_parent(b)
+    
+    if arr[a-1] < arr[b-1] :
+        S[b] = a
+    else :
+        S[a] = b
+        
+def is_set(a,b) :
+    a = get_parent(a)
+    b = get_parent(b)
+    return a == b
+
+for _ in range(M) :
+    a,b = map(int, input().split())
+    union(a,b)
+
+S = list(set(map(get_parent, S[1:])))
+ans = 0
+for s in S :
+    ans += arr[s-1]
+
+if ans > k :
+    print('Oh no')
+else : print(ans)
+
