e568da293dd44c5e4cdf791d249d9f83c3f39e22
Hosung <an3735297@pusan.ac.kr>
2023-03-17 16:07:34 +0900

    BOK_9465(dp/ down-top) fix

--- Algorithm/BOJ_9465_downTop.py
+++ Algorithm/BOJ_9465_downTop.py
@@ -5,8 +5,10 @@ for _ in range(T):
     for _ in range(2):
         arr.append(list(map(int, input().split())))
     dp = [[0 for _ in range(N + 1)] for _ in range(2)]
-
-    for i in range(1, N + 1):
+    
+    dp[0][1] = arr[0][0]
+    dp[1][1] = arr[1][0]
+    for i in range(2, N + 1):
         dp[0][i] = arr[0][i - 1] + max(dp[1][i - 1], dp[1][i - 2])
         dp[1][i] = arr[1][i - 1] + max(dp[0][i - 1], dp[0][i - 2])
 
